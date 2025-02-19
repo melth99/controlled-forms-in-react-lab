c7a6f6d8bd4cb37e5a9fa90d8a986c07e2ca6569
mel-th <meleyth@gmail.com>
2025-02-18 23:48:16 -0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
-// src/App.jsx
 import Bookshelf from "./components/Bookshelf/Bookshelf";
+
 const App = () => {
-  function handleInputChange{
-
-  }
-
-  function handleSubmit{
-    
-  }
-  
   return (
-    <div className="bookshelfDiv">
-    <div className="formDiv">
-      <Bookshelf/>
-      <h3>Add a Book</h3>
-      {/* Form will go here */}
+    <div className="App">
+      <h1>My Bookshelf</h1>
+      <Bookshelf />
     </div>
-    <div className="bookCardsDiv">{/* Book cards will display here */}</div>
-  </div>
-  
-    )
+  );
 };
 
 export default App;
--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,23 +1,55 @@
 import { useState } from 'react';
-// https://generalassembly.instructure.com/courses/733/assignments/16642?module_item_id=64370
 
 const Bookshelf = () => {
     const [books, setBooks] = useState([
         { title: 'Fourth Wing', author: 'Rebecca Yarros' },
         { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
     ]);
-// handleInputChange
+    const [newBook, setNewBook] = useState({ title: '', author: '' });
+
+    function handleChange(e) {
+        setNewBook({ ...newBook, [e.target.name]: e.target.value });
+    }
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        setBooks([...books, newBook]);
+        setNewBook({ title: '', author: '' });
+    }
+
     return (
-        <>
-            <div className="bookshelfDiv">
-                <div className="formDiv">
-                    <h3>Add a Book</h3>
-                    {/* Form will go here */}
-                </div>
-                <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+        <div className="bookshelfDiv">
+            <div className="formDiv">
+                <h3>Add a Book</h3>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor='author'>Author: </label>
+                    <input 
+                        type="text" 
+                        name="author" 
+                        id="author" 
+                        value={newBook.author} 
+                        onChange={handleChange} 
+                    />
+                    <label htmlFor='title'>Title: </label>
+                    <input 
+                        type="text" 
+                        name="title" 
+                        id="title" 
+                        value={newBook.title} 
+                        onChange={handleChange}
+                    />
+                    <button type="submit">Add Book</button>
+                </form>
             </div>
-        </>
-    )
+            <div className="bookCardsDiv">
+                <ul>
+                    {books.map((book, index) => (
+                        <li key={index}>{book.title} : {book.author}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
-export default Bookshelf
+export default Bookshelf;
